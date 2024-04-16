bbe3bf2d00af34998a348f1b3c448200ebbcc19f
Altrono <hbotema16@gmail.com>
2024-04-16 11:23:33 +0200

    Implement SuperQuality  UI  section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,9 +6,9 @@ const Button = ({label, iconURL}) => {
     gap-2 px-7 py-4 border font-montserrat text-lg leading-none
      bg-coral-red rounded-full text-white border-coral-red'>
         {label}
-        <img 
+        {iconURL && <img 
             className='ml-2 rounded-full w-5 h-5'
-            src={iconURL} alt="Arrow right icon" />
+            src={iconURL} alt="Arrow right icon" />}
     </button>
   )
 }

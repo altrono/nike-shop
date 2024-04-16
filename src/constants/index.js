1276d6dc15c5cd1c2468f2dde43cebfb66bb30e7
Altrono <hbotema16@gmail.com>
2024-04-16 14:57:23 +0200

    Implement Footer  UI  section

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,8 +111,8 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "hbotema16@gmail.com", link: "mailto:hbotema16@gmail.com" },
+            { name: "+27818304662", link: "tel:+27 81 830 4662" },
         ],
     },
 ];
